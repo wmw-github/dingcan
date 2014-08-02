a1632c23c9867696a97773570803dbe79c620f4c
zanglaowei <zanglaowei@163.com>
2014-08-02 11:08:06 +0800

    修改首页
    修改首页

--- web/Lib/Action/IndexAction.class.php
+++ web/Lib/Action/IndexAction.class.php
@@ -5,35 +5,34 @@ class IndexAction extends CommonAction {
 
 
     public function index(){
-      //$Food=M('Food');//实例化Food数据模型
-	  //$foodlist=$Food->limit(8)->order('fid desc')->select();//首页显示8个图片
-	  //$this->assign('foodlist',$foodlist);
-	  $Shop=D('Shop');
-      $shoplist=$Shop->limit(8)->order('sid desc')->select();//8个最新商家
-      $shoplisttop=$Shop->limit(8)->where('ssort=99')->order('sid desc')->select();//8个推荐
-      $this->assign('shoplist',$shoplist);
-      $this->assign('shoplisttop',$shoplisttop);
-	  $Link=M('Link');
-	  $llist=$Link->where('type=0')->limit(10)->order('lid desc')->select();//最多显示10个友链
-	  $this->assign('llist',$llist);
-	 // dump($foodlist);
-	  $this->display();
-   }
+	     // $Shop=D('Shop');
+      //  $shoplist=$Shop->limit(8)->order('sid desc')->select();//8个最新商家
+      //  $shoplisttop=$Shop->limit(8)->where('ssort=99')->order('sid desc')->select();//8个推荐
+      //  $this->assign('shoplist',$shoplist);
+      //  $this->assign('shoplisttop',$shoplisttop);
+	     // $Link=M('Link');
+	     // $llist=$Link->where('type=0')->limit(10)->order('lid desc')->select();//最多显示10个友链
+	     // $this->assign('llist',$llist);
+	     $Area=D("area");
+       $arealist=$Area->select();
+       $this->assign('arealist',$arealist);
+	     $this->display();
+    }
 
 	 public function flist(){
-	 $data['fcid']=I('id');//食品分类
-	 $fcid=I('id');
+	   $data['fcid']=I('id');//食品分类
+	   $fcid=I('id');
      $Food=D('Foodcat');
-	 $foodcatlist=$Food->select();
+	   $foodcatlist=$Food->select();
 	 //根据id获取分类名
-	 $foodcatname=$Food->where("fcid='$fcid'")->getField('fcname');
-	 $this->assign('foodcatname',$foodcatname);
-	 $this->assign('foodcatlist',$foodcatlist);
-      $Food=M('Food');
-	  $foodlist=$Food->where($data)->select();
-	  $this->assign('foodlist',$foodlist);
-	 // dump($foodlist);
-	 $this->display();
+	   $foodcatname=$Food->where("fcid='$fcid'")->getField('fcname');
+	   $this->assign('foodcatname',$foodcatname);
+	   $this->assign('foodcatlist',$foodcatlist);
+     $Food=M('Food');
+	   $foodlist=$Food->where($data)->select();
+	   $this->assign('foodlist',$foodlist);
+	   // dump($foodlist);
+	   $this->display();
    }
    public function farealist(){
 	$data['farea']=I('id');//地区分类
